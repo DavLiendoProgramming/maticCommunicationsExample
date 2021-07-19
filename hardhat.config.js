6ffc71db6cdac72d235eca07730efb6521ead7df
David Liendo <dliendo05@gmail.com>
2021-07-19 12:26:54 -0400

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,60 @@
+/**
+ * Required imports
+ */
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-truffle5');
+
+/**
+ * Import private key of the account with funds
+ */
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  /**
+   * using matic network for deployment
+   */
+  defaultNetwork: 'matic',
+  networks: {
+    hardhat: {},
+    matic: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [PRIVATE_KEY],
+      /**
+       * Configuring gas restrictions
+       */
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+    goerli: {
+      url: 'https://goerli.infura.io/v3/8173bc023d8f45e595df4f3fb55db36e',
+      accounts: [PRIVATE_KEY],
+      /**
+       * Configuring gas restrictions
+       */
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  mocha: {
+    timeout: 20000,
+  },
+};
---
+++ scripts/mint.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat');
+async function main() {
+  const [signer] = await ethers.getSigners();
+  console.log('Signer address', signer.address);
+  /**
+   * Contract factory
+   */
+  const NFT = await hre.ethers.getContractFactory('Creature3');
+  // const URI = 'something';
+
+  /**
+   * Owner of the nft and contract addresses
+   */
+  const WALLET_ADDRESS = '0x7e693A3E0132Fa9D24B883979d4e405A64D67361';
+  const CONTRACT_ADDRESS = '0x05E77Cb43d55EbFF082a7426166774ca853c51BB';
+
+  /**
+   * Returns a new instance of the Contract attached to a new address
+   */
+  const contract = NFT.attach(CONTRACT_ADDRESS);
+
+  /**
+   * Minting a new NFT
+   */
+  const txResult = await contract.mintTo(WALLET_ADDRESS);
+  console.log('Tx Result:', txResult);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
